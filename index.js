49e21ef7f3231b61e4fa934668cc5afae0495b6a
gc6123 <gc6123@bard.edu>
2018-11-01 01:59:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -154,9 +154,9 @@ function moveDodgerLeft() {
    */
   //var dodger = positionToInteger(dodger.style.left);
   //     expect(positionToInteger(dodger.style.left)).toBeLessThan(left)
-  const left = positionToInteger(DODGER.style.left);
+  const left = positionToInteger(dodger.style.left);
   function almost() {
-    DODGER.style.left = `${left - 4}px`;
+    dodger.style.left = `${left - 4}px`;
   }
     if (left >= 0) {
   window.requestAnimationFrame(almost);
